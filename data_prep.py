49875eccf7f4ace57d9478bcdd67d7136cf0c881
Matthew Pham <matthewpham03@gmail.com>
2026-07-16 19:54:36 -0700

    Implemented code to establish out for each method

--- data_prep.py
+++ data_prep.py
@@ -1,17 +1,23 @@
 import pandas as pd
+import numpy as np
 
 DATA_PATH = 'data/results.csv'
 
 def load_data(path: str) -> pd.DataFrame:
-    pass
-
-def clean_data(path: pd.DataFrame) -> pd.DataFrame:
-    pass
+    results = pd.read_csv(DATA_PATH)
+    results['date'] = pd.to_datetime(results['date'])
+    return results
 
 def summarize(df: pd.DataFrame) -> None:
-    pass
+    total_rows = len(df)
+    min_val = df['date'].min()
+    max_val = df['date'].max()
+    unique_teams = len(np.unique(df[['home_team', 'away_team']]))
 
+    print('Total Rows:', total_rows)
+    print('Date Range:', min_val, 'to', max_val)
+    print('Number of Unique Teams:', unique_teams)
+    
 if __name__ == "__main__":
     df = load_data(DATA_PATH)
-    df = clean_data(df)
     summarize(df)
