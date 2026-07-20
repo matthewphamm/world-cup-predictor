2bdfc14e554424a6fe5baa0e795f709aae8d0017
Matthew Pham <matthewpham03@gmail.com>
2026-07-20 12:34:38 -0700

    Add helper functions to compute elo

--- elo.py
+++ elo.py
@@ -5,3 +5,16 @@ DATA_PATH = 'data/results.csv'
 STARTING_RATE = 1500 # can be changed
 K_FACTOR = 20 # can be changed
 
+def expected_score(rating_a: float, rating_b: float) -> float: # type: ignore
+    pass
+
+def actual_score(home_score: int, away_score: int) -> float: # type: ignore
+    pass
+
+def update_ratings(rating_home: float, rating_away: float,
+                   home_score: int, away_score: int, 
+                    k: float = K_FACTOR) -> tuple[float, float]: # type: ignore
+    pass
+
+def build_ratings(df: pd.DataFrame) -> dict[str, float]: # type: ignore
+    pass
