ba3fe8b1f47911bfc8bed357ea59a2210c0c5f6a
Matthew Pham <matthewpham03@gmail.com>
2026-07-20 21:00:51 -0700

    Delete

--- test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from unittest.mock import patch
-from predict import predict_match
-
-class TestPredictMatch(unittest.TestCase):
-    pass
-
-if __name__ == "__main__":
-    unittest.main()
