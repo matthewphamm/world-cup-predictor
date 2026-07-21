dd86d8fb2c716b86e5dd83f75dd11bca617223ae
Matthew Pham <matthewpham03@gmail.com>
2026-07-21 11:56:44 -0700

    Implement code to print out description of trained data

--- train_data.py
+++ train_data.py
@@ -5,3 +5,8 @@ from elo import build_ratings, STARTING_RATING
 def build_training_df(df: pd.DataFrame) -> pd.DataFrame:
     pass
 
+if __name__ == "__main__":
+    df = load_data(DATA_PATH)
+    training_df = build_training_df(df)
+    print(training_df.head())
+    print(training_df.describe())
