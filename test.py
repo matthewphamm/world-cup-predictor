41c2b8df03dc6f7f243b31300fcea9e73d5e3411
Matthew Pham <matthewpham03@gmail.com>
2026-07-20 19:43:52 -0700

    Add imports and create class

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+from unittest.mock import patch
+from predict import predict_match
+
+class TestPredictMatch(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
